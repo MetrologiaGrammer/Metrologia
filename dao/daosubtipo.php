9ee2f335b5006030d5a43aa02d8ca55661ad5baa
Adrian <adrian.aragon@grammer.com>
2023-10-31 11:26:32 -0600

    actualizacion

--- dao/daosubtipo.php
+++ dao/daosubtipo.php
@@ -1,24 +1,24 @@
 
+
 <?php
 
 include_once('db.php');
 
-$area=$_GET['area'];
+Contador();
 
-Contador($area);
-
-function Contador($area){
+function Contador(){
 
     $con = new LocalConector();
 
     $conex=$con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT `Sub-Tipo` FROM `Tipos`where `Tipo` = '$area' GROUP by `Sub-Tipo`;");
+    $datos = mysqli_query($conex, "SELECT `Sub-Tipo` FROM `Tipos` GROUP BY `Sub-Tipo`;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
 
     echo json_encode(array("data"=>$resultado));
 
 }
+
 
 ?>
