8d26fcc885537b27e20dc833ecda0a9a405d2bcf
Adrian <adrian.aragon@grammer.com>
2023-11-27 17:02:07 -0600

    actualizacion

--- inicio/actualizarEquipo.php
+++ inicio/actualizarEquipo.php
@@ -326,11 +326,11 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"]== null) {
         const min = 1;
         const max = 1000000;
         const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-        
+
         console.log(foto);
 
         Nomina = '<?php echo $_SESSION["nomina"];?>';
-        AreaProceso = document.getElementById("procceso").value;
+        AreaProceso = document.getElementById("proceso").value;
         Linea = document.getElementById("linea").value;
         Operacion = document.getElementById("operacion").value;
         IdEquipo = document.getElementById("codigo").value;
