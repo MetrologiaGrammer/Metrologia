7f1118156d9492a81d677896f9c16617a6b13cfb
Adrian <adrian.aragon@grammer.com>
2023-11-27 15:47:14 -0600

    actualizacion

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -22,7 +22,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"]== null) {
 
 <header id="header">
     <div class="inner">
-        <img src="images/00001472.jpg" alt="" style="width: 30%"/>
+        <img src="images/nominas/<?php echo $_SESSION["nomina"];?>.jpg" alt="" style="width: 30%"/>
         <h1>Nomina</h1>
         <h1><strong><?php echo $_SESSION["nomina"];?></strong>.</h1>
         <h1>Nombre</h1>
