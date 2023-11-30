c91ffbdf429ef8df3db5c5c947692b91a30be75c
Adrian <adrian.aragon@grammer.com>
2023-11-30 14:51:15 -0600

    actualizacion

--- inicio/dao/daoConsultafoto.php
+++ inicio/dao/daoConsultafoto.php
@@ -12,7 +12,7 @@ function Contador($idreferncia){
     $con = new LocalConector();
 
     $conex=$con->conectar();
-
+    echo "SELECT * FROM `Historialcambios` WHERE `IdEquipo` = '$idreferncia' ORDER BY `IdHistorial` DESC LIMIT 1;";
     $datos = mysqli_query($conex, "SELECT * FROM `Historialcambios` WHERE `IdEquipo` = '$idreferncia' ORDER BY `IdHistorial` DESC LIMIT 1;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
