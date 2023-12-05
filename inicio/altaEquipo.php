0efb63f3b978c4b01aea6c2769e36f49d87ac8f5
Adrian <adrian.aragon@grammer.com>
2023-12-04 20:44:14 -0600

    INICIO

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -445,7 +445,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
                         .then(function (response) {
                             if (response.ok) {
                                 alert("done");
-                                enviarCorreo(NOMINA, "", PROCESO, LINEA, REFERENCIA, OPERACION,OBSERVACIONES);
+                                enviarCorreo(NOMINA, "", PROCESO, LINEA, REFERENCIA, OPERACION,OBSERVACIONES,TIPO);
                             } else {
                                 throw "Error";
                             }
@@ -483,7 +483,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
     }
 
 
-    function enviarCorreo(nomina, Retiqueta, proceso, linea, Referencia, operacion,observaciones) {
+    function enviarCorreo(nomina, Retiqueta, proceso, linea, Referencia, operacion,observaciones,tipo) {
 
         //document.getElementById("carga").style.display="block";
         //document.getElementById("contenidoReporte").style.display="none";
@@ -497,6 +497,9 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
         data.append('operacion', operacion);
         data.append('Idimagen', "");
         data.append('comentario', observaciones);
+        data.append('tipi', tipo);
+
+
         fetch('https://arketipo.mx//MailerMetrologiaIng.php', {
             method: 'POST',
             body: data
