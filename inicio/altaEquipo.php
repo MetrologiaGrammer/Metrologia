c3d03573db2ea7d4e8e4840d5d118cac60c4304e
Adrian <adrian.aragon@grammer.com>
2023-12-04 20:16:07 -0600

    INICIO

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -445,7 +445,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
                         .then(function (response) {
                             if (response.ok) {
                                 alert("done");
-                                enviarCorreo(NOMINA, PROCESO, LINEA, REFERENCIA, OPERACION);
+                                enviarCorreo(NOMINA, REFERENCIA, PROCESO, LINEA, OPERACION,OBSERVACIONES);
                             } else {
                                 throw "Error";
                             }
