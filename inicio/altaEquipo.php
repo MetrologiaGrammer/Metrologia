a12ed1939b1adcaca6c425f0635ffb102b622a08
Adrian <adrian.aragon@grammer.com>
2023-12-04 21:01:02 -0600

    INICIO

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -445,7 +445,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
                         .then(function (response) {
                             if (response.ok) {
                                 alert("done");
-                                enviarCorreo(NOMINA, "", PROCESO, LINEA, REFERENCIA, OPERACION,OBSERVACIONES,TIPO);
+                                enviarCorreo(NOMINA, "", PROCESO, LINEA, REFERENCIA, OPERACION,OBSERVACIONES,TIPO,SUBTIPO,FECHACALIBRACION,FECHAVENCIDA,RESOLUCION,RANGO,FRECUENCIA,NUMSERIE,NUMPARTE,STATUS);
                             } else {
                                 throw "Error";
                             }
@@ -483,7 +483,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
     }
 
 
-    function enviarCorreo(nomina, Retiqueta, proceso, linea, Referencia, operacion,observaciones,tipo) {
+    function enviarCorreo(nomina, Retiqueta, proceso, linea, Referencia, operacion,observaciones,tipo,subtipo,fechacaibracion,fechavencida,resolucion,rango,frecuencia,numserie,numparte,estatus) {
 
         //document.getElementById("carga").style.display="block";
         //document.getElementById("contenidoReporte").style.display="none";
@@ -498,7 +498,15 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
         data.append('Idimagen', "");
         data.append('comentario', observaciones);
         data.append('tipo', tipo);
-
+        data.append('subtipo', subtipo);
+        data.append('fechacalibracion', fechacalibracion);
+        data.append('fechavencida', fechavencida);
+        data.append('resolucion', resolucion);
+        data.append('rango', rengo);
+        data.append('frecuencia', frecuencia);
+        data.append('numserie', numserie);
+        data.append('numparte', numparte;
+        data.append('status', estatus);
 
         fetch('https://arketipo.mx//MailerMetrologiaIng.php', {
             method: 'POST',
