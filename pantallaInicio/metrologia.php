a290f7c568998c363931bdabeee3f7099760a1d4
Adrian <adrian.aragon@grammer.com>
2024-01-15 08:20:11 -0600

    INICIO

--- pantallaInicio/metrologia.php
+++ pantallaInicio/metrologia.php
@@ -27,7 +27,7 @@
 							<ul>
 
 								<li><a href="https://arketipo.mx/Metrologia/inicio/altaEquipo.php" class="icon brands fa-dribbble">Ingresar<span class="label">Dribbble</span></a></li>
-								<li><a href="https://arketipo.mx/Metrologia/estudiosnum2.html" class="icon brands fa-dribbble">Certificados<span class="label">Dribbble</span></a></li>
+								<li><a href="https://arketipo.mx/Metrologia/estudiosnum2.html" class="icon brands fa-dribbble">Centro de costo<span class="label">Dribbble</span></a></li>
 								<li><a href="https://arketipo.mx/Metrologia/inicio/editarEquipo.php" class="icon brands fa-dribbble">EDITAR<span class="label">Dribbble</span></a></li>
 							</ul>
 						</nav>
