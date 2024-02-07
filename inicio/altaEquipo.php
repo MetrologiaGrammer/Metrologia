3f2ceaa42cbd3185b33a9953bef71009e676f347
Adrian <adrian.aragon@grammer.com>
2024-02-07 12:19:56 -0600

    INICIO

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -144,7 +144,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
                 </div>
             </div>
 
-            <hr>
+            <br>
 
             <div class="col-6 col-12-xsmall" id="campoResolucion">
                 <label for="referencia" style="text-align: center">Resolucion</label>
