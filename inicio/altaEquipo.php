5a073942e8813b58b11a5479769b34947f463fea
Adrian <adrian.aragon@grammer.com>
2024-02-22 18:09:11 -0600

    INIC

--- inicio/altaEquipo.php
+++ inicio/altaEquipo.php
@@ -332,25 +332,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
             }
         });
 
-        let campos = [, "campoOperacion""campoNpersona", "campoPersona"];
 
-        if (select === "PERSONAL" ) {
-            for (let i = 0; i < campos.length; i++) {
-                if (campos[i] === "campoNpersona" || campos[i] === "campoPersona") {
-                    document.getElementById(campos[i]).style.display = 'block';
-                } else {
-                    document.getElementById(campos[i]).style.display = 'none';
-                }
-            }
-        } else {
-            for (let i = 0; i < campos.length; i++) {
-                if (campos[i] === "campoNpersona" || campos[i] === "campoPersona") {
-                    document.getElementById(campos[i]).style.display = 'none';
-                } else {
-                    document.getElementById(campos[i]).style.display = 'block';
-                }
-            }
-        }
     }
 
 
