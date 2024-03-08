f01a0c4759b87e2f500166491c6a5a763715ddf4
Adrian <adrian.aragon@grammer.com>
2024-03-08 15:36:11 -0600

    INIC

--- inicio/dao/daoguardarEquipo.php
+++ inicio/dao/daoguardarEquipo.php
@@ -41,8 +41,8 @@ function cliente($Nomina, $Nombre,$Area,$Referencia,$Tipo,$Subtipo,$Proceso,$Lin
 {
     $con = new LocalConector();
     $conexion = $con->conectar();
-    $consP = "UPDATE `Equipo` SET `Observaciones`='$Observaciones',`Status`='$Status',`Resolucion`='$Resolucion',`NumParte`='$Numparte',`Descripcion`='',`Nominal`='',`TolMax`='',`TolMin`='',`EspMin`='',`EspMax`='',`Caracteristica`='',`FechaInspeccion`='$Fechacalibracion',`Frecuencia`='$Frecuencia',`FechaVencimiento`='$Fechavencida',`AreaProceso`='$Proceso',`Tipo`='$Tipo',`SubTipo`='$Subtipo',`NumSerie`='$Numserie',`AreaLinea`='$Linea',`AreaOperacion`='$Operacion',`Rango`='$Rango' WHERE `IdEquipo`='$Referencia'";
-     $rsconsPro = mysqli_query($conexion, $consP);
+    $consP = "UPDATE `Equipo` SET `Observaciones`='$Observaciones',`Status`='$Status',`Resolucion`='$Resolucion',`NumParte`='$Numparte',`Descripcion`='',`Nominal`='',`TolMax`='',`TolMin`='',`EspMin`='',`EspMax`='',`Caracteristica`='',`FechaInspeccion`='$Fechacalibracion',`Frecuencia`='$Frecuencia',`FechaVencimiento`='$Fechavencida',`AreaProceso`='$Proceso',`Tipo`='$Tipo',`SubTipo`='$Subtipo',`NumSerie`='$Numserie',`AreaLinea`='$Linea',`AreaOperacion`='$Operacion',`Rango`='$Rango',`Ntarget`='',`CodColor`='',`ClienteMaster`='',`PlataformaMaster`='',`VendedorMaster`='',`NombreColorMaster`='',`NumColorMaster`='',`NominaPersona`='',`NombrePersona`='' WHERE `IdEquipo`='$Referencia'";
+    $rsconsPro = mysqli_query($conexion, $consP);
     mysqli_close($conexion);
 
     $imagenCodificada = $Imagen; //ejemplo de imagen en base64
