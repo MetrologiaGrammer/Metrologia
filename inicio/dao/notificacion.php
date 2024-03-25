ebf768ed3aab6012805cef4b4d2b6e1d76219d19
Adrian <adrian.aragon@grammer.com>
2024-03-25 16:43:10 -0600

    INIC

--- inicio/dao/notificacion.php
+++ inicio/dao/notificacion.php
@@ -11,51 +11,58 @@ function Contador(){
     $datos = mysqli_query($conex, "SELECT * FROM `Equipo` WHERE Notificacion = 0 and STR_TO_DATE(    CONCAT('01-',        CASE            WHEN `FechaVencimiento` LIKE '%enero%' THEN REPLACE(`FechaVencimiento`, 'enero', 'january')           WHEN `FechaVencimiento` LIKE '%febrero%' THEN REPLACE(`FechaVencimiento`, 'febrero', 'february')            WHEN `FechaVencimiento` LIKE '%marzo%' THEN REPLACE(`FechaVencimiento`, 'marzo', 'march')            WHEN `FechaVencimiento` LIKE '%abril%' THEN REPLACE(`FechaVencimiento`, 'abril', 'april')            WHEN `FechaVencimiento` LIKE '%mayo%' THEN REPLACE(`FechaVencimiento`, 'mayo', 'may')            WHEN `FechaVencimiento` LIKE '%junio%' THEN REPLACE(`FechaVencimiento`, 'junio', 'june')            WHEN `FechaVencimiento` LIKE '%julio%' THEN REPLACE(`FechaVencimiento`, 'julio', 'july')            WHEN `FechaVencimiento` LIKE '%agosto%' THEN REPLACE(`FechaVencimiento`, 'agosto', 'august')            WHEN `FechaVencimiento` LIKE '%septiembre%' THEN REPLACE(`FechaVencimiento`, 'septiembre', 'september')            WHEN `FechaVencimiento` LIKE '%octubre%' THEN REPLACE(`FechaVencimiento`, 'octubre', 'october')            WHEN `FechaVencimiento` LIKE '%noviembre%' THEN REPLACE(`FechaVencimiento`, 'noviembre', 'november')            WHEN `FechaVencimiento` LIKE '%diciembre%' THEN REPLACE(`FechaVencimiento`, 'diciembre', 'december')        END    ), '%d-%M-%y') < CURDATE();");
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
 
+    $html = "<table border='1'>";
+    $html .= "<tr>";
+    $html .= "<th>ID</th>";
+    $html .= "<th>Area Proceso</th>";
+    $html .= "<th>Tipo</th>";
+    $html .= "<th>SubTipo</th>";
+    $html .= "<th>Area Linea</th>";
+    $html .= "<th>Area Operacion</th>";
+    $html .= "</tr>";
+
     foreach($resultado as $fila){
 
-            $datos_post = array(
-                'idequipo' => $fila['IdEquipo'],
-                'observaciones' => $fila['Observaciones'],
-                'status' => $fila['Status'],
-                'resolucion' => $fila['Resolucion'],
-                'numParte' => $fila['NumParte'],
-                'fechaInspeccion' => $fila['FechaInspeccion'],
-                'frecuencia' => $fila['Frecuencia'],
-                'fechaVencimiento' => $fila['FechaVencimiento'],
-                'areaProceso' => $fila['AreaProceso'],
-                'tipo' => $fila['Tipo'],
-                'subTipo' => $fila['SubTipo'],
-                'numSerie' => $fila['NumSerie'],
-                'areaLinea' => $fila['AreaLinea'],
-                'areaOperacion' => $fila['AreaOperacion'],
-                'rango' => $fila['Rango'],
-                'centroCosto' => $fila['CentroCosto'],
-                'costoCalibracion' => $fila['CostoCalibracion'],
+        $html .= "<tr>";
+        $html .= "<td>" . $fila['IdEquipo'] . "</td>";
+        $html .= "<td>" . $fila['AreaProceso'] . "</td>";
+        $html .= "<td>" . $fila['Tipo'] . "</td>";
+        $html .= "<td>" . $fila['SubTipo'] . "</td>";
+        $html .= "<td>" . $fila['AreaLinea'] . "</td>";
+        $html .= "<td>" . $fila['AreaOperacion'] . "</td>";
+        $html .= "</tr>";
 
-            );
 
-            // URL del archivo PHP a ejecutar
-            $url = 'https://arketipo.mx/MailerMetrologiaNotificacion.php';
-
-            // Inicializar cURL
-            $curl = curl_init($url);
-
-            // Configurar opciones de cURL
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datos_post));
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-            // Ejecutar la solicitud y obtener la respuesta
-            $respuesta = curl_exec($curl);
-
-            // Cerrar la sesión de cURL
-            curl_close($curl);
-
-            // Imprimir la respuesta
-            echo $respuesta;
-
-             cliente($fila['IdEquipo']);
         }
+
+    $html .= "</table>";
+
+    $datos_post = array(
+        'html' => $html
+
+    );
+
+    // URL del archivo PHP a ejecutar
+    $url = 'https://arketipo.mx/MailerMetrologiaNotificacion.php';
+
+    // Inicializar cURL
+    $curl = curl_init($url);
+
+    // Configurar opciones de cURL
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($datos_post));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    // Ejecutar la solicitud y obtener la respuesta
+    $respuesta = curl_exec($curl);
+
+    // Cerrar la sesión de cURL
+    curl_close($curl);
+
+    // Imprimir la respuesta
+    echo $respuesta;
+
+    cliente($fila['IdEquipo']);
 
 }
 
