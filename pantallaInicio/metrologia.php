acbde22a43435ca3b277b773ee69d580e7d2dd5e
Adrian <adrian.aragon@grammer.com>
2024-04-02 09:35:59 -0600

    INIC

--- pantallaInicio/metrologia.php
+++ pantallaInicio/metrologia.php
@@ -34,8 +34,9 @@
 						<nav>
 							<ul>
 
-								<li><a href="https://arketipo.mx/Metrologia/inicio/altaEquipo.php" class="icon brands fa-dribbble">Ingresar<span class="label">Dribbble</span></a></li>
-                                <li><a href="https://arketipo.mx/Metrologia/inicio/editarEquipo.php" class="icon brands fa-dribbble">Editar<span class="label">Dribbble</span></a></li>
+
+                                <li style="margin-right: 10px;" class=""><a href="https://arketipo.mx/Metrologia/inicio/altaEquipo.php" target="_blank" ><img class="zoom" style="width: 100%" alt="" src="imagen/registro.png"></a></li>
+                                <li style="margin-right: 10px;" class=""><a href="https://arketipo.mx/Metrologia/inicio/editarEquipo.php" target="_blank" ><img class="zoom" style="width: 100%" alt="" src="imagen/editar.png"></a></li>
 								<li><a href="https://arketipo.mx/Metrologia/estudiosnum2.html" class="icon brands fa-dribbble">Costos<span class="label">Dribbble</span></a></li>
                                 <li style="margin-right: 10px;" class=""><a href="https://arketipo.mx/Metrologia/estudios.html" target="_blank" ><img class="zoom" style="width: 100%" alt="" src="imagen/estudios.png"></a></li>
 							</ul>
