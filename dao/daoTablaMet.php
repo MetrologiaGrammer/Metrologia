18a637b0822cbd0ab8d551461f9f105624f5a4c0
Adrian <adrian.aragon@grammer.com>
2024-04-08 09:46:34 -0600

    Inicio de gradicas

---
+++ dao/daoTablaMet.php
@@ -0,0 +1,24 @@
+
+<?php
+
+include_once('db.php');
+
+Contador();
+
+function Contador(){
+
+    $con = new LocalConector();
+
+    $conex=$con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT `IdEquipo`,`Tipo`,`AreaProceso`,`AreaLinea`,`AreaOperacion`,`FechaVencimiento`,`FechaInspeccion`,`EspMin`,`EspMax`,`Observacion`,CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/editarEquipo.php?ID=',`IdEquipo`,'\" class=\"btn btn-primary\">Editar</a>') AS boton FROM `Equipo` WHERE 1;");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+
+    echo json_encode(array("data"=>$resultado));
+
+}
+
+
+?>
+
