683fe8624d94d8d3067f47a69d64d89b05fcad93
Adrian <adrian.aragon@grammer.com>
2024-05-23 17:23:10 -0600

    Creacion de nuevos tipos

--- inicio/dao/daoingresotipo.php
+++ inicio/dao/daoingresotipo.php
@@ -2,18 +2,18 @@
 
 include_once('db.php');
 
-$Proceso = $_POST['Proceso'];
-$Linea = $_POST['Linea'];
-$Operacion= $_POST['Operacion'];
+$Ref = $_POST['Ref'];
+$Tipo = $_POST['Tipo'];
+$Subtipo= $_POST['SubTipo'];
 
 
-cliente($Proceso,$Linea,$Operacion);
+cliente($Ref,$Tipo,$Subtipo);
 
-function cliente($Proceso,$Linea,$Operacion)
+function cliente($Ref,$Tipo,$Subtipo)
 {
     $con = new LocalConector();
     $conexion = $con->conectar();
-    $consP = "INSERT INTO `Areas`(`Proceso`, `Linea`, `OpEstacion`) VALUES ('$Proceso','$Linea','$Operacion')";
+    $consP = "INSERT INTO `Tipo`(`Ref`, `Tipo`, `SubTipo`) VALUES ('$Ref','$Tipo','$Subtipo')";
     $rsconsPro = mysqli_query($conexion, $consP);
     mysqli_close($conexion);
 }
