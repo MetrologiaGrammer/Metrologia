61fd79c34f1963c145758d8712d96940e0a3f628
Adrian <adrian.aragon@grammer.com>
2024-05-24 13:39:15 -0600

    Creacion de nuevos tipos

--- dao/daoTabla.php
+++ dao/daoTabla.php
@@ -45,7 +45,7 @@ $query = "
 
             CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/index.html?ID=', `IdEquipo`, '\" class=\"btn btn-primary\">ENTRAR</a>') AS boton ,
        
-            CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/index.html?ID=', `IdEquipo`, '\" class=\"btn btn-primary\">ENTRAR</a>') AS boton2 
+            CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/consultaEquipo.html?ID=', `IdEquipo`, '\" class=\"btn btn-primary\">ENTRAR</a>') AS boton2 
 
         FROM 
 
