f2fee50573995b7a81269da6eaa9f21591610e20
Adrian <adrian.aragon@grammer.com>
2024-05-27 15:51:31 -0600

    Creacion de nuevos tipos

--- dao/daoTabla.php
+++ dao/daoTabla.php
@@ -3,59 +3,47 @@
 
 include_once('db.php');
 
-Contador();
+$fechaCalibracion=$_GET['fechaCalibracion'];
+$fechaVencimiento=$_GET['fechaVencimiento'];
 
-function Contador(){
+Contador($fechaCalibracion,$fechaVencimiento);
+
+function Contador($fechaCalibracion,$fechaVencimiento){
 
     $con = new LocalConector();
 
     $conex=$con->conectar();
 
-$query = "
+    $query = "
+            SELECT 
+                `IdEquipo`,
+                `Tipo`,
+                `AreaProceso`,
+                `AreaLinea`,
+                `FechaInspeccion`,
+                `FechaVencimiento`,
+                `EspMin`,
+                `EstadoCalibracion`,
+                CASE 
+                    WHEN `EstadoCalibracion` = 0 THEN '<span class=\"badge badge-pill badge-primary\" style=\"background: goldenrod;\">On time</span>'
+                    WHEN `EstadoCalibracion` = 1 THEN '<span class=\"badge badge-pill badge-success\" style=\"background: green;\">Completed</span>'
+                    WHEN `EstadoCalibracion` = 2 THEN '<span class=\"badge badge-pill badge-warning\" style=\"background: goldenrod;\">On time</span>'
+                    ELSE '<span class=\"badge badge-pill badge-danger\" style=\"background: red;\">late</span>'
+                END AS `EstatusCalibracion`,
+                CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/index.html?ID=', `IdEquipo`, '\" class=\"btn btn-info\">Document</a>') AS boton ,
+                CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/consultaEquipo.html?ID=', `IdEquipo`, '\" class=\"btn btn-secondary\">Information</a>') AS boton2 
+            FROM 
+                `Equipo` 
+            WHERE 
+                1=1 ";
 
-        SELECT 
+    if ($fechaCalibracion != "n"){
+        $query=$query." and FechaInspeccion = '$fechaCalibracion' ";
+    }
 
-            `IdEquipo`,
-
-            `Tipo`,
-
-            `AreaProceso`,
-
-            `AreaLinea`,
-
-            `FechaInspeccion`,
-
-            `FechaVencimiento`,
-
-            `EspMin`,
-
-            `EstadoCalibracion`,
-
-            CASE 
-
-                WHEN `EstadoCalibracion` = 0 THEN '<span class=\"badge badge-pill badge-primary\" style=\"background: goldenrod;\">On time</span>'
-
-                WHEN `EstadoCalibracion` = 1 THEN '<span class=\"badge badge-pill badge-success\" style=\"background: green;\">Completed</span>'
-
-                WHEN `EstadoCalibracion` = 2 THEN '<span class=\"badge badge-pill badge-warning\" style=\"background: goldenrod;\">On time</span>'
-
-                ELSE '<span class=\"badge badge-pill badge-danger\" style=\"background: red;\">late</span>'
-
-            END AS `EstatusCalibracion`,
-
-            CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/index.html?ID=', `IdEquipo`, '\" class=\"btn btn-info\">Document</a>') AS boton ,
-       
-            CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/consultaEquipo.html?ID=', `IdEquipo`, '\" class=\"btn btn-secondary\">Information</a>') AS boton2 
-
-        FROM 
-
-            `Equipo` 
-
-        WHERE 
-
-            1;
-
-    ";
+    if ($fechaVencimiento != "n"){
+        $query=$query." and FechaVencimiento = '$fechaVencimiento' ";
+    }
 
     $datos = mysqli_query($conex, $query);
 
