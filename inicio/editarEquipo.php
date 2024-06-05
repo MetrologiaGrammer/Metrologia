8a7d216beb29c62157819eca35b9d9278e0f8ddb
Adrian <adrian.aragon@grammer.com>
2024-06-05 16:02:35 -0600

    Creacion de nuevos tipos

--- inicio/editarEquipo.php
+++ inicio/editarEquipo.php
@@ -201,7 +201,7 @@ if ($_SESSION["nomina"] == "" && $_SESSION["nomina"] == null) {
         <div class="col-6 col-12-xsmall">
             <ul class="actions stacked" style="text-align: center">
                 <li><a class="button solid" onclick="testActualizacion()">Guardar Cambio</a></li>
-                <li><a class="btn btn-danger" onclick="eliminarequipo()">Eliminar equipo</a></li>
+                <li><a class="btn btn-danger" onclick="eliminarequipo()">Eliminar Equipo</a></li>
             </ul>
         </div>
 
