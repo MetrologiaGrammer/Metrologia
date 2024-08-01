2fff3f90ebb08bdbd58ae967ec36db444fb750fd
Adrian <adrian.aragon@grammer.com>
2024-08-01 09:23:48 -0600

    Creacion de nuevos tipos

--- dao/daoTablaMet.php
+++ dao/daoTablaMet.php
@@ -11,7 +11,7 @@ function Contador(){
 
     $conex=$con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT `IdEquipo`,`Observaciones`,`Tipo`,`AreaProceso`,`AreaLinea`,`AreaOperacion`,`FechaInspeccion`,`FechaVencimiento`,`EspMin`,`EspMax`,CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/editarEquipo.php?ID=',`IdEquipo`,'\" class=\"btn btn-SUCCESS\">EDITAR</a>') AS boton FROM `Equipo` WHERE 1;");
+    $datos = mysqli_query($conex, "SELECT `IdEquipo`,`Observaciones`,`Status`,`Tipo`,`AreaProceso`,`AreaLinea`,`AreaOperacion`,`FechaInspeccion`,`FechaVencimiento`,`EspMin`,`EspMax`,CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/editarEquipo.php?ID=',`IdEquipo`,'\" class=\"btn btn-SUCCESS\">EDITAR</a>') AS boton FROM `Equipo` WHERE 1;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
 
