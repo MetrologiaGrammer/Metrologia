38bab5620037043c491c4cc080af63f51415dca2
Adrian <adrian.aragon@grammer.com>
2024-08-12 12:17:06 -0600

    Creacion de nuevos tipos

---
+++ dao/daoTablaMaster.php
@@ -0,0 +1,24 @@
+
+<?php
+
+include_once('db.php');
+
+Contador();
+
+function Contador(){
+
+    $con = new LocalConector();
+
+    $conex=$con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT `IdEquipo`,`NumParte`,`Plataforma`,`Cliente`,`Vendedor`,`Material`,`NombColor`,`NumColor`,`CodColor`,`Status`,`NumTarget`,`Vencimiento`,CONCAT('<a href=\"https://arketipo.mx/Metrologia/inicio/editarEquipo.php?ID=',`IdEquipo`,'\" class=\"btn btn-SUCCESS\">EDITAR</a>') AS boton FROM `Master` WHERE 1;");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+
+    echo json_encode(array("data"=>$resultado));
+
+}
+
+
+?>
+
